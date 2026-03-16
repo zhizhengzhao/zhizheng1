b0c452f84044dd44102739889552f364ead18a39
zhizhengzhao <zhizhengzhao@outlook.com>
2026-03-16 10:35:03 +0800

    chore: point training to server datasets

--- particle_transformer/env.sh
+++ particle_transformer/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DATADIR_JetClass=
-export DATADIR_TopLandscape=
-export DATADIR_QuarkGluon=
+export DATADIR_JetClass=/miaojiawei/zhizheng/data/Jet/JetClass
+export DATADIR_TopLandscape=/miaojiawei/zhizheng/data/TopLandscape
+export DATADIR_QuarkGluon=/miaojiawei/zhizheng/data/QuarkGluon
--- particle_transformer/train_JetClass.sh
+++ particle_transformer/train_JetClass.sh
@@ -62,19 +62,6 @@ fi
 # currently only Pythia
 SAMPLE_TYPE=Pythia
 
-# [TODO] test_20M 下载完成后，把下面这段加回到 $CMD 命令中 --data-val 和 --data-config 之间：
-#    --data-test \
-#    "HToBB:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToBB_*.root" \
-#    "HToCC:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToCC_*.root" \
-#    "HToGG:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToGG_*.root" \
-#    "HToWW2Q1L:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToWW2Q1L_*.root" \
-#    "HToWW4Q:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToWW4Q_*.root" \
-#    "TTBar:${DATADIR}/${SAMPLE_TYPE}/test_20M/TTBar_*.root" \
-#    "TTBarLep:${DATADIR}/${SAMPLE_TYPE}/test_20M/TTBarLep_*.root" \
-#    "WToQQ:${DATADIR}/${SAMPLE_TYPE}/test_20M/WToQQ_*.root" \
-#    "ZToQQ:${DATADIR}/${SAMPLE_TYPE}/test_20M/ZToQQ_*.root" \
-#    "ZJetsToNuNu:${DATADIR}/${SAMPLE_TYPE}/test_20M/ZJetsToNuNu_*.root" \
-
 $CMD \
     --data-train \
     "HToBB:${DATADIR}/${SAMPLE_TYPE}/train_100M/HToBB_*.root" \
@@ -88,6 +75,17 @@ $CMD \
     "ZToQQ:${DATADIR}/${SAMPLE_TYPE}/train_100M/ZToQQ_*.root" \
     "ZJetsToNuNu:${DATADIR}/${SAMPLE_TYPE}/train_100M/ZJetsToNuNu_*.root" \
     --data-val "${DATADIR}/${SAMPLE_TYPE}/val_5M/*.root" \
+    --data-test \
+    "HToBB:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToBB_*.root" \
+    "HToCC:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToCC_*.root" \
+    "HToGG:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToGG_*.root" \
+    "HToWW2Q1L:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToWW2Q1L_*.root" \
+    "HToWW4Q:${DATADIR}/${SAMPLE_TYPE}/test_20M/HToWW4Q_*.root" \
+    "TTBar:${DATADIR}/${SAMPLE_TYPE}/test_20M/TTBar_*.root" \
+    "TTBarLep:${DATADIR}/${SAMPLE_TYPE}/test_20M/TTBarLep_*.root" \
+    "WToQQ:${DATADIR}/${SAMPLE_TYPE}/test_20M/WToQQ_*.root" \
+    "ZToQQ:${DATADIR}/${SAMPLE_TYPE}/test_20M/ZToQQ_*.root" \
+    "ZJetsToNuNu:${DATADIR}/${SAMPLE_TYPE}/test_20M/ZJetsToNuNu_*.root" \
     --data-config data/JetClass/JetClass_${FEATURE_TYPE}.yaml --network-config $modelopts \
     --model-prefix training/JetClass/${SAMPLE_TYPE}/${FEATURE_TYPE}/${model}/{auto}${suffix}/net \
     $dataopts $batchopts \
